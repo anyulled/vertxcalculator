0da14868a9cd0ebcacd7c01c193319154508d900
alrs <anyul.rivas@gft.com>
2017-09-28 19:35:48 +0200

    removing ignored tests

--- src/test/java/org/superbiz/CalculatorVerticleTest.java
+++ src/test/java/org/superbiz/CalculatorVerticleTest.java
@@ -7,7 +7,6 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.RunTestOnContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,7 +21,6 @@ public class CalculatorVerticleTest {
     public RunTestOnContext rule = new RunTestOnContext();
 
     @Test
-    @Ignore
     public void should_add_two_numbers(TestContext context) {
 
         // given
--- src/test/java/org/superbiz/InfrastructureTest.java
+++ src/test/java/org/superbiz/InfrastructureTest.java
@@ -7,7 +7,6 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.RunTestOnContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,7 +20,6 @@ public class InfrastructureTest {
     public RunTestOnContext rule = new RunTestOnContext();
 
     @Test
-    @Ignore
     public void should_add_two_numbers(TestContext context) {
 
         // given
